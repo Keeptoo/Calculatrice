046b4ff479c73ca20c246040df6a1a38713dda0f
Keeptoo <nathanweber1706@gmail.com>
2023-01-07 16:25:46 +0100

    first commit

---
+++ assets/Js/main.js
@@ -0,0 +1,28 @@
+let bouton = [...document.querySelectorAll('.key')]
+let keycode = bouton.map(bouton => bouton.dataset.key)
+let firstpart = document.querySelector('.firstpart')
+
+addEventListener('click', (e) => {
+    let value = e.target.dataset.key;
+    calcul(value)
+})
+
+function calcul(value) {
+    if (keycode.includes(value)) {
+        switch (value) {
+            case '8':
+                firstpart.textContent = '';
+                break;
+            case '188':
+                let calcule = eval(firstpart.textContent);
+                firstpart.textContent = calcule;
+                break;
+            default:
+                let indexkeycode = keycode.indexOf(value)
+                let boutons = bouton[indexkeycode]
+                firstpart.textContent += boutons.innerHTML;
+                break;
+        }
+    }
+}
+
